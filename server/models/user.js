bb5e9aab70d04a650feb003fbfcc5a881163e437
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-01-17 23:34:38 -0500

    Set default profile picture

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
-  picture: String,//link to picture
+  picture: {
+    type: String,
+    default:
+      "https://www.anitawatkins.com/wp-content/uploads/2016/02/Generic-Profile-1600x1600.png",
+  }, //link to picture
 });
 
 module.exports = mongoose.model("user", UserSchema);
