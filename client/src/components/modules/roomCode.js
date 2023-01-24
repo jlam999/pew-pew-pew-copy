6be59b705ac563ee5419910cd4a9dfc58f275ed5
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-01-24 17:00:19 -0500

    other roomcode error

--- client/src/components/modules/roomCode.js
+++ client/src/components/modules/roomCode.js
@@ -1,56 +1,56 @@
 import React, { useState } from "react";
 
-import "./RoomCode.css"
+import "./roomCode.css";
 
 /**
  * The box in the home page that allows you to input the room code
- * 
+ *
  * The following props are used:
  * @param {Function} onSubmit //NOT PASSED IN YET, MUST BE RESOLVED
  */
 
 const NewCodeInput = (props) => {
-    const [roomCode,setRoomCode] = useState("");
+  const [roomCode, setRoomCode] = useState("");
 
-    const handleChange = (event) => {
-        setRoomCode(event.target.roomCode)
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.onSubmit && props.onSubmit(code);
-        setValue("");
-    };
+  const handleChange = (event) => {
+    setRoomCode(event.target.roomCode);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit && props.onSubmit(code);
+    setValue("");
+  };
 
-    return (
-        <div className="largeContainer">
-        <div className="roomCodeContainer"> 
-            <input 
-                type="text"
-                value={roomCode}
-                placeholder="Enter Code"
-                onChange={handleChange}
-                minLength = "6"
-                maxLength = "6"
-                className="roomCodeInput"
-            />
-            <button
-                type="submit"
-                value="Join" //TO BE CHANGED: VALUE THAT IS SUBMITTED UPON PRESS
-                onClick={handleSubmit}
-                className="joinButton"
-            >
-                Join
-            </button>
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="largeContainer">
+      <div className="roomCodeContainer">
+        <input
+          type="text"
+          value={roomCode}
+          placeholder="Enter Code"
+          onChange={handleChange}
+          minLength="6"
+          maxLength="6"
+          className="roomCodeInput"
+        />
+        <button
+          type="submit"
+          value="Join" //TO BE CHANGED: VALUE THAT IS SUBMITTED UPON PRESS
+          onClick={handleSubmit}
+          className="joinButton"
+        >
+          Join
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const ReadCode = () => {
-    const attemptCode = (value) => {
-        //Try to query a room or something
-    }
-    <NewCodeInput defaultText="Enter Code" onSubmit={attemptCode}/>
-}
+  const attemptCode = (value) => {
+    //Try to query a room or something
+  };
+  <NewCodeInput defaultText="Enter Code" onSubmit={attemptCode} />;
+};
 
 export default NewCodeInput;
