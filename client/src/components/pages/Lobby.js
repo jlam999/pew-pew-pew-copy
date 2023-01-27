ae0e4af6cdd866ad56f2ca960cbc6862d8ba2cf5
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-01-27 12:28:01 -0500

    adjusted not found page

--- client/src/components/pages/Lobby.js
+++ client/src/components/pages/Lobby.js
@@ -1,76 +1,71 @@
-import { Redirect, Link, useNavigate } from "@reach/router"
-import React, {useState, useEffect} from "react";
+import { Redirect, Link, useNavigate } from "@reach/router";
+import React, { useState, useEffect } from "react";
 import { socket } from "../../client-socket.js";
 import { get, post } from "../../utilities.js";
 import { handleKeyDown, handleKeyUp, handleClick } from "../../input.js";
-import PlayerBox from "../modules/PlayerBox.js"
+import PlayerBox from "../modules/PlayerBox.js";
 
-import "./Lobby.css"
+import "./Lobby.css";
 
 const Lobby = (props) => {
-    const navigate = useNavigate()
-    const [playerList, setPlayerList] = useState([]);
+  const navigate = useNavigate();
+  const [playerList, setPlayerList] = useState([]);
 
-    const getPlayers = () => {
-        get("/api/gameState").then((gameState) => {
-            const promiseArray = []
-            for (let playerId of Object.keys(gameState.players)) {
-                let user = get("/api/user", {googleid: playerId})
-                promiseArray.push(user)
-            }
-            Promise.all(promiseArray).then((array) => {
-                let names = array.map((player) => {
-                    return player.name;
-                })
-                setPlayerList(names);
-            })
-        })
-    }
+  const getPlayers = () => {
+    get("/api/gameState").then((gameState) => {
+      const promiseArray = [];
+      for (let playerId of Object.keys(gameState.players)) {
+        let user = get("/api/user", { googleid: playerId });
+        promiseArray.push(user);
+      }
+      Promise.all(promiseArray).then((array) => {
+        let names = array.map((player) => {
+          return player.name;
+        });
+        setPlayerList(names);
+      });
+    });
+  };
 
-    useEffect(() => {
-        getPlayers();
-    }, [])
+  useEffect(() => {
+    getPlayers();
+  }, []);
 
-    // console.log(players)
+  // console.log(players)
 
-    useEffect(() => {
-        document.title = "Lobby"
-        socket.on("start game", () =>{
-            window.addEventListener("keydown", handleKeyDown);
-            window.addEventListener("keyup", handleKeyUp);
-            window.addEventListener("click", handleClick);
-            navigate("/game");
-        })
-    }, [])
+  useEffect(() => {
+    document.title = "Lobby";
+    socket.on("start game", () => {
+      navigate("/game");
+    });
+  }, []);
 
-    const attemptGameStart = () => {
-        get("/api/gameState").then((gameState)=> {
-            console.log(gameState)
-            if (gameState.isActive) {
-                alert("Game in Session; Cannot Join.");
-            } else if (Object.keys(gameState.players).length < 2) {
-                alert("Not enough players!")
-            } else {
-                post("/api/startGame", {});
-            }
-        })
-    }
+  const attemptGameStart = () => {
+    get("/api/gameState").then((gameState) => {
+      console.log(gameState);
+      if (gameState.isActive) {
+        alert("Game in Session; Cannot Join.");
+      } else if (Object.keys(gameState.players).length < 2) {
+        alert("Not enough players!");
+      } else {
+        post("/api/startGame", {});
+      }
+    });
+  };
 
-    const players = playerList.map((playerName) => (
-        <PlayerBox name={playerName}/>
-    ))
+  const players = playerList.map((playerName) => <PlayerBox name={playerName} />);
 
-    return (
-        <>
-            <h1 className="Lobby-title">Game Lobby</h1>
-            <h2 className="Lobby-code">Code: ABCXYZ</h2> {/*TO BE REPLACED WITH ROOM CODES*/}
-            <p>Players:</p>
-            {players}
-            <button className="Lobby-startButton" onClick={attemptGameStart}>
-                Start Game
-            </button>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="Lobby-title">Game Lobby</h1>
+      <h2 className="Lobby-code">Code: ABCXYZ</h2> {/*TO BE REPLACED WITH ROOM CODES*/}
+      <p>Players:</p>
+      {players}
+      <button className="Lobby-startButton" onClick={attemptGameStart}>
+        Start Game
+      </button>
+    </>
+  );
+};
 
 export default Lobby;
