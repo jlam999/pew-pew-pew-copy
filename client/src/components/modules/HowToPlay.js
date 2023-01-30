1fa1a5da2fcefe4cb1caea39d367de7052cf26e5
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-01-30 17:12:47 -0500

    Started working on how to play

---
+++ client/src/components/modules/HowToPlay.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../utilities.css";
+
+const HowToPlay = () => {
+  return (
+    <div className="u-flex">
+      <div>Use WASD to move</div>
+      <div>Use mouse to aim and shoot</div>
+      <div>Use the space bar to pull your bullets back</div>
+    </div>
+  );
+};
+
+export default HowToPlay;
