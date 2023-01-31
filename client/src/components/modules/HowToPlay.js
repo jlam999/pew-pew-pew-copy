048a0353036ad7ee6f7466fda2b072bdca9e29db
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-01-31 14:29:47 -0500

    better animation

--- client/src/components/modules/HowToPlay.js
+++ client/src/components/modules/HowToPlay.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 import "../../utilities.css";
+import "./HowToPlay.css";
+import "../pages/Lobby.css";
 
 const HowToPlay = () => {
   return (
-    <div className="u-flexColumn">
-      <div className="HowToPlay-Box">Use WASD to move</div>
-      <div className="HowToPlay-Box">Use mouse to aim and shoot</div>
-      <div className="HowToPlay-Box">Use the space bar to pull your bullets back</div>
+    <div>
+      <input type="checkbox" id="openSidebar" />
+      <label className="HowToPlay-Label" for="openSidebar">
+        How To Play
+      </label>
+      <div id="HowToPlay-Menu">
+        <div className="HowToPlay-Box">Use WASD or Arrow Keys to move</div>
+        <div className="HowToPlay-Box">
+          Use mouse to aim and shoot. Be careful though! Every time you shoot, your health decreases
+          slightly.
+        </div>
+        <div className="HowToPlay-Box">
+          Move over bullets or press and hold the space bar to retrieve you bullets.
+        </div>
+        <div className="HowToPlay-Box">
+          There are health packs on the field that help you regain health!
+        </div>
+      </div>
     </div>
   );
 };
