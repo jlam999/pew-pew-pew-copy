705b1b24263f89d4a3b021d700a6d2e4fcdfe776
caiti008 <caiti008@gmail.com>
2023-02-01 02:38:46 -0500

    Added instruction videos

--- client/src/components/modules/HowToPlay.js
+++ client/src/components/modules/HowToPlay.js
@@ -12,16 +12,21 @@ const HowToPlay = () => {
         <p className="HowToPlay-Text">How To Play</p>
       </label>
       <div id="HowToPlay-Menu">
-        <div className="HowToPlay-Box">Use WASD or Arrow Keys to move</div>
         <div className="HowToPlay-Box">
-          Use mouse to aim and shoot. Be careful though! Every time you shoot, your health decreases
-          slightly.
+          <p className="HowToPlay-Instructions">Use WASD or Arrow Keys to move your base.</p>
+          <video src="./instrMove.mp4" autoPlay muted loop type="video/mp4" className="demoVid1"/>
         </div>
         <div className="HowToPlay-Box">
-          Move over bullets or press and hold the space bar to retrieve you bullets.
+          <p className="HowToPlay-Instructions">Use mouse to aim and shoot. However, shooting decreases your health slightly.</p>
+          <video src="./instrShoot.mp4" autoPlay muted loop type="video/mp4" className="demoVid1"/>     
         </div>
         <div className="HowToPlay-Box">
-          There are health packs on the field that help you regain health!
+          <p className="HowToPlay-Instructions">Move over bullets or press and hold the space bar to retrieve you bullets.</p>
+          <video src="./instrPull.mp4" autoPlay muted loop type="video/mp4" className="demoVid1"/>
+        </div>
+        <div className="HowToPlay-Box">
+          <p className="HowToPlay-Instructions">There are health packs on the field that help you regain health!</p>
+          <video src="./instrHealth.mp4" autoPlay muted loop type="video/mp4" className="demoVid1"/>
         </div>
       </div>
     </div>
