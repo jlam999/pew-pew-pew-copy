f5d10f4c6349151d968d79c09e36fa2dac1f3de2
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-02-03 02:14:28 -0500

    can't press profile button when not logged in

--- client/src/components/modules/ProfileButton.js
+++ client/src/components/modules/ProfileButton.js
@@ -1,17 +1,18 @@
-import React, { useState,useEffect } from "react";
-import {GoogleLogin, GoogleOAuthProvider, googleLogout } from "@react-oauth/google";
+import React, { useState, useEffect } from "react";
+import { GoogleLogin, GoogleOAuthProvider, googleLogout } from "@react-oauth/google";
 import { Link } from "@reach/router";
 import { get } from "../../utilities";
 
-import "./ProfileButton.css"
+import "./ProfileButton.css";
 
-const defaultProfilePicture = "https://www.anitawatkins.com/wp-content/uploads/2016/02/Generic-Profile-1600x1600.png"
+const defaultProfilePicture =
+  "https://www.anitawatkins.com/wp-content/uploads/2016/02/Generic-Profile-1600x1600.png";
 
 const GOOGLE_CLIENT_ID = "329655122590-a18c4j9pbbiu6thahct4a0dse0p457qd.apps.googleusercontent.com"; //OUR CLIENT, NOT WEBLAB'S
 
 /**
  * Profile Button rendered in the home page
- * 
+ *
  * The following props are used:
  * @param {Function} handleLogin for Google Login
  * @param {Function} handleLogout for Google Logout
@@ -19,63 +20,58 @@ const GOOGLE_CLIENT_ID = "329655122590-a18c4j9pbbiu6thahct4a0dse0p457qd.apps.goo
  */
 
 const ProfileButton = (props) => {
-    const [user, setUser] = useState("")
-    const [isLoggedIn, setStatus] = useState(false);
-    useEffect(() => {
-        get("/api/user", { googleid: props.userId }).then((user) => {
-            if (user.length === 0) {
-                setStatus(false)
-            } else {
-                setUser(user);
-                setStatus(true)
-            }
-        });
-    }, [props.userId]);
-    
+  const [user, setUser] = useState("");
+  const [isLoggedIn, setStatus] = useState(false);
+  useEffect(() => {
+    get("/api/user", { googleid: props.userId }).then((user) => {
+      if (user.length === 0) {
+        setStatus(false);
+      } else {
+        setUser(user);
+        setStatus(true);
+      }
+    });
+  }, [props.userId]);
 
-    return (
-        <div>
-            <div className="googleLoginButton">
-            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-                {props.userId ? (<button
-                    onClick={() => {
-                        googleLogout();
-                        props.handleLogout();
-                    }}
-                    className="googleLogoutButton"
-                >
-                    Logout
-                </button>) : (
-                    <GoogleLogin
-                        text="signin"
-                        onSuccess={props.handleLogin}
-                        onError={(err)=>console.log(err)}
-                        className="googleLoginButton"
-                        theme={"outline"}
-                        shape="pill"
-                        ux_mode={"popup"}
-                        width="96px"
-                    />
-                )}
-            </GoogleOAuthProvider>
-            </div>
-            <button className="profileImgButton">
-                <Link to={`/profile/${props.userId}`}>
-                    {isLoggedIn ? (<img
-                        src={user.picture}
-                        alt="Profile" 
-                        className="profileImg"
-                    />
-                    )   :   (<img
-                        src={defaultProfilePicture}
-                        alt="Profile" 
-                        className="profileImg"
-                    />
-                    )}
-                </Link>
+  return (
+    <div>
+      <div className="googleLoginButton">
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          {props.userId ? (
+            <button
+              onClick={() => {
+                googleLogout();
+                props.handleLogout();
+              }}
+              className="googleLogoutButton"
+            >
+              Logout
             </button>
-        </div>
-    )
-}
+          ) : (
+            <GoogleLogin
+              text="signin"
+              onSuccess={props.handleLogin}
+              onError={(err) => console.log(err)}
+              className="googleLoginButton"
+              theme={"outline"}
+              shape="pill"
+              ux_mode={"popup"}
+              width="96px"
+            />
+          )}
+        </GoogleOAuthProvider>
+      </div>
+      <button className="profileImgButton">
+        <Link to={props.userId ? `/profile/${props.userId}` : "/"}>
+          {isLoggedIn ? (
+            <img src={user.picture} alt="Profile" className="profileImg" />
+          ) : (
+            <img src={defaultProfilePicture} alt="Profile" className="profileImg" />
+          )}
+        </Link>
+      </button>
+    </div>
+  );
+};
 
 export default ProfileButton;
